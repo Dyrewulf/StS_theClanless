666abc8b676811f0f5a28bca362db68ebc5bf15a
Dyrewulf <edward.mcglynn@yahoo.com>
2021-01-21 17:18:09 -0800

    adding images for future cards

---
+++ theClanless/src/main/java/theClanless/powers/UndeadPersistencePower.java
@@ -0,0 +1,85 @@
+package theClanless.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.OnPlayerDeathPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import com.megacrit.cardcrawl.powers.IntangiblePower;
+import theClanless.theClanlessMod;
+import theClanless.util.TextureLoader;
+
+import static theClanless.theClanlessMod.makePowerPath;
+
+//Gain 1 dex for the turn for each card played.
+
+public class UndeadPersistencePower extends AbstractPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = theClanlessMod.makeID("UndeadPersistencePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("UndeadPersistencePower84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("UndeadPersistencePower32.png"));
+
+    public UndeadPersistencePower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public float atDamageReceive(float damage, DamageInfo.DamageType damageType) {
+        int currentHP = AbstractDungeon.player.currentHealth;
+
+        if (damage > currentHP) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new ApplyPowerAction(owner, owner, new IntangiblePower(owner, 3), 3)
+            );
+            AbstractDungeon.actionManager.addToBottom(
+                    new ReducePowerAction(owner, owner, this, 1)
+            );
+            return 1;
+
+        }
+        return damage;
+    }
+
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + this.amount;
+    }
+
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new UndeadPersistencePower(owner, source, amount);
+    }
+}
