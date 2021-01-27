541dce1a5507a62af42e6ee87d5688ee4eedf753
Dorrinal <dorrinal@hotmail.com>
2021-01-27 14:15:03 -0800

    Fixed cost display for Blur+ and Cat Burglary+.

--- theClanless/src/main/java/theClanless/cards/Blur.java
+++ theClanless/src/main/java/theClanless/cards/Blur.java
@@ -15,44 +15,32 @@ import static theClanless.theClanlessMod.makeCardPath;
 
 public class Blur extends AbstractDynamicCard {
 
-    // public static final String ID = DefaultMod.makeID(${NAME}.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
-    public static final String ID = theClanlessMod.makeID("Blur"); // DELETE THIS ONE.
-    public static final String IMG = makeCardPath("Blur.png");// "public static final String IMG = makeCardPath("${NAME}.png");
-    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
-
-
-    // /TEXT DECLARATION/
-
+    public static final String ID = theClanlessMod.makeID("Blur");
+    public static final String IMG = makeCardPath("Blur.png");
 
     // STAT DECLARATION
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
-    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
-    private static final CardType TYPE = CardType.ATTACK;       //
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheClanless.Enums.CELERITY;
 
-    private static final int COST = 1;  // COST = ${COST}
-    private static final int UPGRADED_COST = 1; // UPGRADED_COST = ${UPGRADED_COST}
-
-    private static final int DAMAGE = 5;    // DAMAGE = ${DAMAGE}
+    private static final int COST = 1;
+    private static final int DAMAGE = 5;
     private static final int MAGICNUMBER = 2;
     private static final int MAGICNUMBER_PLUS = 1;
-
-
     // /STAT DECLARATION/
 
-
-    public Blur() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+    public Blur() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.damage = this.baseDamage = DAMAGE;
         this.magicNumber = this.baseMagicNumber = MAGICNUMBER;
 
         this.tags.add(CardTags.STRIKE);
     }
-
 
     // Actions the card should do.
     @Override
@@ -63,13 +51,11 @@ public class Blur extends AbstractDynamicCard {
         }
     }
 
-
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADED_COST);
             upgradeMagicNumber(MAGICNUMBER_PLUS);
             initializeDescription();
         }
--- theClanless/src/main/java/theClanless/cards/CatBurglary.java
+++ theClanless/src/main/java/theClanless/cards/CatBurglary.java
@@ -1,7 +1,6 @@
 package theClanless.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainGoldAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -18,45 +17,32 @@ import static theClanless.theClanlessMod.makeCardPath;
 
 public class CatBurglary extends AbstractDynamicCard {
 
-    // public static final String ID = DefaultMod.makeID(${NAME}.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
-    public static final String ID = theClanlessMod.makeID("CatBurglary"); // DELETE THIS ONE.
-    public static final String IMG = makeCardPath("CatBurglary.png");// "public static final String IMG = makeCardPath("${NAME}.png");
-    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
-
-
-    // /TEXT DECLARATION/
-
+    public static final String ID = theClanlessMod.makeID("CatBurglary");
+    public static final String IMG = makeCardPath("CatBurglary.png");
 
     // STAT DECLARATION
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
-    private static final CardRarity RARITY = CardRarity.RARE; //  Up to you, I like auto-complete on these
-    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
-    private static final CardType TYPE = CardType.ATTACK;       //
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheClanless.Enums.CELERITY;
 
-    private static final int COST = 2;  // COST = ${COST}
-    private static final int UPGRADED_COST = 2; // UPGRADED_COST = ${UPGRADED_COST}
-
+    private static final int COST = 2;
     private static final int DAMAGE = 12;
     private static final int UPGRADE_PLUS_DMG = 6;
     private static final int MAGICNUMBER = 2;
     private static final int MAGICNUMBER_PLUS = 1;
-
-
     // /STAT DECLARATION/
 
-
-    public CatBurglary() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+    public CatBurglary() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.damage = this.baseDamage = DAMAGE;
         this.magicNumber = this.baseMagicNumber = MAGICNUMBER;
-
         this.exhaust = true;
     }
-
 
     // Actions the card should do.
     @Override
@@ -65,7 +51,7 @@ public class CatBurglary extends AbstractDynamicCard {
                 new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)
         );
         CardCrawlGame.sound.play("GOLD_JINGLE");
-        for(int i = 0; i < this.magicNumber * this.damage; ++i) {
+        for (int i = 0; i < this.magicNumber * this.damage; ++i) {
             AbstractDungeon.effectList.add(new GainPennyEffect(m.hb.cX, m.hb.cY));
         }
         AbstractDungeon.actionManager.addToBottom(
@@ -73,16 +59,13 @@ public class CatBurglary extends AbstractDynamicCard {
         );
     }
 
-
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeBaseCost(UPGRADED_COST);
             upgradeMagicNumber(MAGICNUMBER_PLUS);
-            this.rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
