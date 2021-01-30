3479aae6ef9a430f8d952cc63ef9c400ccb41ebe
Dorrinal <dorrinal@hotmail.com>
2021-01-29 18:30:02 -0800

    Added vulnerable to Target Vitals.

--- theClanless/src/main/java/theClanless/cards/TargetVitals.java
+++ theClanless/src/main/java/theClanless/cards/TargetVitals.java
@@ -1,11 +1,12 @@
 package theClanless.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import theClanless.characters.TheClanless;
 import theClanless.theClanlessMod;
 
@@ -42,6 +43,7 @@ public class TargetVitals extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, this.magicNumber, false)));
     }
 
     // Upgraded stats.
